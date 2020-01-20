4e7217e15669dd88f8ef94eb5a22aaa23f6fdc5b
Farhaaaan11 <faisalfarhaaan@gmail.com>
2020-01-20 13:02:05 +0600

    update

---
+++ BMI.m
@@ -0,0 +1,180 @@
+function varargout = BMI(varargin)
+% BMI MATLAB code for BMI.fig
+%      BMI, by itself, creates a new BMI or raises the existing
+%      singleton*.
+%
+%      H = BMI returns the handle to a new BMI or the handle to
+%      the existing singleton*.
+%
+%      BMI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in BMI.M with the given input arguments.
+%
+%      BMI('Property','Value',...) creates a new BMI or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before BMI_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to BMI_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help BMI
+
+% Last Modified by GUIDE v2.5 16-Jan-2020 18:56:27
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @BMI_OpeningFcn, ...
+                   'gui_OutputFcn',  @BMI_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before BMI is made visible.
+function BMI_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to BMI (see VARARGIN)
+
+% Choose default command line output for BMI
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes BMI wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = BMI_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+
+function age_Callback(hObject, eventdata, handles)
+% hObject    handle to age (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of age as text
+%        str2double(get(hObject,'String')) returns contents of age as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function age_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to age (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function height_Callback(hObject, eventdata, handles)
+% hObject    handle to height (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of height as text
+%        str2double(get(hObject,'String')) returns contents of height as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function height_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to height (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function weight_Callback(hObject, eventdata, handles)
+% hObject    handle to weight (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of weight as text
+%        str2double(get(hObject,'String')) returns contents of weight as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function weight_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to weight (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function bmi_Callback(hObject, eventdata, handles)
+% hObject    handle to bmi (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of bmi as text
+%        str2double(get(hObject,'String')) returns contents of bmi as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function bmi_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to bmi (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in bmi1.
+function bmi1_Callback(hObject, eventdata, handles)
+
+w = str2double(get(handles.weight,'string'));
+h = str2double(get(handles.height,'string'));
+res = w/h*h;
+
+set(handles.bmi,'string',num2str(res));
+
+
+% hObject    handle to bmi1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
